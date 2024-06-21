74c0675755c4b2aacee494cb191f2dd71505bfdf
terrytsang0618 <terrytsang0618@gmail.com>
2024-06-21 18:08:46 +0800

    Update the Data_Management Template

--- base/data/views/views.py
+++ base/data/views/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-from django.views.generic import TemplateView,ListView,DetailView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from data.models import CustomerProfile
+from data.models import CustomerProfile, CustomerLedger, BalanceChange
 
-
-# Create your views here.
-class HomeView(LoginRequiredMixin,ListView):
-    template_name = 'home2.html'
+class HomeView(LoginRequiredMixin, ListView):
+    template_name = 'home.html'
     login_url = '/admin'
-    
-    # def get(self, request, *args, **kwargs):
-    #     context = self.get_context_data(**kwargs)
-    #     # context['test'] = {'test':'Hello World'}
-    #     return self.render_to_response(context)
-
     model = CustomerProfile
     context_object_name = 'customers'
 
-
-class DetailView(LoginRequiredMixin,DetailView):
+class DetailView(LoginRequiredMixin, DetailView):
     template_name = 'detail.html'
     login_url = '/admin'
     model = CustomerProfile
     context_object_name = 'customer'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['customers'] = CustomerProfile.objects.all()
+        try:
+            context['ledger'] = CustomerLedger.objects.get(customer=context['customer'])
+        except CustomerLedger.DoesNotExist:
+            context['ledger'] = None
+        
+        # Fetch and limit balance change records related to the customer to 10 most recent
+        context['balance_changes'] = BalanceChange.objects.filter(receiver=context['customer']).order_by('-transaction_date')[:10]
+        
+        return context
